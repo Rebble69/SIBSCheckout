fa9461d22672df1c2009669cdb51615b4aab2c10
Rebble69 <69352549+Rebble69@users.noreply.github.com>
2022-05-24 23:32:19 +0700

    pog

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import qs from "qs";
 import {
   Text,
   View,
@@ -8,7 +9,6 @@ import {
   ScrollView,
 } from "react-native";
 import { Camera, BarCodeScanningResult } from "expo-camera";
-import {} from "react-native-paper";
 import StudentCard from "./components/StudentCard";
 import axios from "axios";
 
@@ -23,6 +23,8 @@ export default function App() {
   const [text, setText] = useState<string>("Not yet scanned");
   const [students, setStudents] = useState<IStudent[]>([]);
   const [paused, setPaused] = useState(false);
+
+  const [camera, setCamera] = useState<Camera | null>(null);
 
   const askForCameraPermission = () => {
     (async () => {
@@ -49,12 +51,15 @@ export default function App() {
 
     console.log(rawUrl.split("/")[6]);
 
-    const postBody: Map<string, string> = new Map();
+    const postBody: any = {};
 
     entries.forEach((entry) => {
       const [key, value] = entry.split("=");
-      postBody.set(key, value);
+      postBody[key] = value;
     });
+
+    delete postBody["usp"];
+    const postData = qs.stringify(postBody);
 
     const res = await axios({
       url: `https://docs.google.com/forms/d/e/${
@@ -62,26 +67,17 @@ export default function App() {
       }/formResponse`,
 
       method: "POST",
-      headers: {
-        authority: "docs.google.com",
-        accept:
-          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-language": "en-US,en;q=0.9",
-        "cache-control": "no-cache",
-        "content-type": "application/x-www-form-urlencoded",
-      },
-      data: Object.fromEntries(postBody),
+
+      data: postData,
     });
 
-    if (res.status === 200) {
-      const removalStudentIndex = students.findIndex(
-        (student) => student.rawUrl === rawUrl
-      );
-      setStudents(students.filter((_, index) => index !== removalStudentIndex));
-      return;
-    }
+    console.log(res.status);
 
-    Alert.alert("Error", "Something went wrong, please contact max y9`");
+    const removalStudentIndex = students.findIndex(
+      (student) => student.rawUrl === rawUrl
+    );
+    setStudents(students.filter((_, index) => index !== removalStudentIndex));
+    return;
   };
 
   // What happens when we scan the bar code
@@ -99,8 +95,8 @@ export default function App() {
 
     // if barcode valid
     const newStudent: IStudent = {
-      name: data.split("/")[7].split("=")[2].split("&")[0],
-      parentName: data.split("=")[3],
+      name: data.split("/")[7].split("=")[2].split("&")[0].replace("+", " "),
+      parentName: data.split("=")[3].replace("+", " "),
       rawUrl: data,
     };
 
@@ -139,14 +135,14 @@ export default function App() {
   // Return the View
   return (
     <ScrollView>
-      <View>
+      <View style={styles.camerabox}>
         <Camera
           onBarCodeScanned={!paused ? handleBarCodeScanned : undefined}
-          style={{ width: 400, height: 300 }}
-          ratio="1:1"
+          style={{ width: 480, height: 640 }}
+          ratio="4:3"
         />
       </View>
-      <Text style={styles.maintext}>{text}</Text>
+      {/* <Text style={styles.maintext}>{text}</Text> */}
       {students.map((student) => {
         return (
           <StudentCard
@@ -174,13 +170,10 @@ const styles = StyleSheet.create({
     fontSize: 16,
     margin: 20,
   },
-  barcodebox: {
-    alignItems: "center",
-    justifyContent: "center",
-    height: 300,
-    width: 300,
-    overflow: "hidden",
-    borderRadius: 30,
-    backgroundColor: "tomato",
+  camerabox: {
+    overflow: "visible",
+    width: 600,
+    height: 400,
   },
 });
+``;
